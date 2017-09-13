517f0f1d4be960d74f7779c52e75b8412287b8a5
Vinicius Borba <vinichius@gmail.com>
2017-09-13 11:53:31 -0300

    add class Restaurante

---
+++ src/Restaurante.java
@@ -0,0 +1,12 @@
+
+public class Restaurante {
+
+	Pratos[] pratosDoRestaurante = new Pratos [2];
+	
+	//metodo mostrar o cardápio
+	public Pratos[] mostrarCardapio(){
+		return pratosDoRestaurante;
+	}
+	
+	
+}
