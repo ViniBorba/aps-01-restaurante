987137aee006c01ee2f2a28b57bc5ff342eefa19
Vinicius Borba <vinichius@gmail.com>
2017-09-13 16:45:53 -0300

    Alterado SistemaDePedidos

--- src/Pedido.java
+++ src/Pedido.java
@@ -3,8 +3,9 @@ public class Pedido {
 	
 	public static double valorTotal;
 	
+	Pedido(){
 	Pratos prato1 = new Pratos();
 	Pratos prato2 = new Pratos();
 	Pratos prato3 = new Pratos();
-
+	}
 }
--- src/SistemaDePedidos.java
+++ src/SistemaDePedidos.java
@@ -5,11 +5,8 @@ public class SistemaDePedidos {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//A ideia é que ao inicar a calss Restaurante o construtor inicie a class pratos que tem o construtor também.
-		Restaurante pratosDoRestaurante;
-		pratosDoRestaurante = new Restaurante();
 		
-		pratosDoRestaurante.mostrarCardapio();
+		Pedido novoPedido1 = new Pedido();
 		
 		
 		
