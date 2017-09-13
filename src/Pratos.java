d288466de7c27576bb7448f7390d8b31341b3229
Vinicius Borba <vinichius@gmail.com>
2017-09-13 11:50:41 -0300

    Pimeiro commit. add class Pratos

---
+++ src/Pratos.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Pratos {
+	
+	Scanner scanner = new Scanner(System.in); 
+	
+	private String nomeDoPrato;
+	private double precoDoPrato;
+	private String descricaoDoPrato;
+	
+	Pratos(){
+		System.out.print("Informe o valor do prato: ");
+		precoDoPrato = scanner.nextDouble();
+		
+		System.out.print("Informe o nome do Prato: ");
+		nomeDoPrato = scanner.nextLine();
+	}
+	
+	//metodo set, que vai alterar o preco e a descricao dos pratos.
+	public void setPrecoDoPrato(double precoDoPrato){
+		this.precoDoPrato = precoDoPrato;
+	}
+	
+	public void setDescricaoDoPrato(String descricaoDoPrato){
+		this.descricaoDoPrato = descricaoDoPrato;
+	}
+	
+	//metodo get que vai retornar os valores para serem impressos quando perguntados.
+	public double getprecoDoPrato(){
+		return precoDoPrato;
+	}
+	
+	public String getDescricaoDoPrato(){
+		return descricaoDoPrato;
+	}
+
+}
